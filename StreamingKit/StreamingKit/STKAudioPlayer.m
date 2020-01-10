109630cfbf57594e6117bc9ca22fb7d2b214d20d
Pierre Pegoud <pierre.pegoud@gmail.com>
2020-01-10 11:17:00 +0000

    Removed the delta calculation to fix seeking through m4a content.

--- StreamingKit/StreamingKit/STKAudioPlayer.m
+++ StreamingKit/StreamingKit/STKAudioPlayer.m
@@ -1473,13 +1473,7 @@ AudioFormatListItem GetFirstPlayableAudioFormatForFile(AudioFileStreamID inFileI
         error = AudioFileStreamSeek(audioFileStream, seekPacket, &packetAlignedByteOffset, &ioFlags);
         
         if (!error && !(ioFlags & kAudioFileStreamSeekFlag_OffsetIsEstimated))
-        {
-            double delta = ((seekByteOffset - (SInt64)currentEntry->audioDataOffset) - packetAlignedByteOffset) / calculatedBitRate * 8;
-            
-            OSSpinLockLock(&currentEntry->spinLock);
-            currentEntry->seekTime -= delta;
-            OSSpinLockUnlock(&currentEntry->spinLock);
-            
+        {            
             seekByteOffset = packetAlignedByteOffset + currentEntry->audioDataOffset;
         }
     }
